b675b6f57ed36d57e2ca0af9ffc483af95023b99
Alvin Pon <alvinpon@icloud.com>
2016-01-04 13:58:57 -0800

    first script of PowerShell

---
+++ QueryUsers.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module ActiveDirectory
+
+$SchoolName = 'Cañada' # 'CSM', 'Cañada', 'Skyline'
+$ADUsers = Get-ADUser -SearchBase "OU=$SchoolName,OU=District,DC=smccd,DC=net" -Filter * | Select-Object -ExpandProperty Name
+
+foreach ($ADUser in $ADUsers) {
+    New-Item -Path "C:\Users\ponalvin\Desktop\Active Directory\$ADUser" -ItemType directory
+}
+
+$HomeFolders = Get-ChildItem .\ -Directory
+foreach ($HomeFolder in $HomeFolders) {
+    $Path = $HomeFolder.FullName
+    $Username = $HomeFolder.Name
+    $Acl = Get-Acl -Path $Path
+    $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("SMCNET\ponalvin", "FullControl", "ContainerInherit", "None", "Allow")
+    $Acl.SetAccessRule($Ar)
+    Set-Acl $Path -AclObject $Acl
+}
