a99c21d3687da323eba1aa9c9c0b2103f234c028
Alvin Pon <alvinpon@icloud.com>
2016-01-11 10:21:26 -0800

    1. Adding new Variables-sample.ps1 file

---
+++ Variables-sample.ps1
@@ -0,0 +1,7 @@
+﻿$username   = "username"
+$password   = "password" | ConvertTo-SecureString -AsPlainText -Force
+$searchBase = "ou=ou,dc=dc,dc=dc"
+$server     = "server"
+$domain     = "domain"
+$path       = "path"
+$credential = New-Object -ArgumentList $username, $password -TypeName System.Management.Automation.PSCredential
