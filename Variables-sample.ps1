e598a3a4cf1d6f3d3ae9e91fb7bac914f127ec16
Alvin Pon <alvinpon@icloud.com>
2016-01-11 16:03:53 -0800

    1. Adding user input for choosing company 2. Filtering Company, EmployeeID and ObjectClass attributes 3. Moving all variables into Variables-sample.ps1 file

--- Variables-sample.ps1
+++ Variables-sample.ps1
@@ -1,7 +1,11 @@
-﻿$username   = "username"
-$password   = "password" | ConvertTo-SecureString -AsPlainText -Force
-$searchBase = "ou=ou,dc=dc,dc=dc"
-$server     = "server"
-$domain     = "domain"
-$path       = "path"
-$credential = New-Object -ArgumentList $username, $password -TypeName System.Management.Automation.PSCredential
+﻿$username              = "username"
+$password              = "password" | ConvertTo-SecureString -AsPlainText -Force
+$company               = "company"
+$searchBase            = "ou=ou,dc=dc,dc=dc"
+$server                = "server"
+$domain                = "domain"
+$path                  = "path"
+$fileSystemAccessRight = [System.Security.AccessControl.FileSystemRights]::FullControl
+$inheritanceFlag       = [System.Security.AccessControl.InheritanceFlags]::None
+$propagationFlag       = [System.Security.AccessControl.PropagationFlags]::None
+$accessControlType     = [System.Security.AccessControl.AccessControlType]::Allow
