0a10bcd40021b21394cd63077f4ad9a03bb1bcba
Alvin Pon <alvinpon@icloud.com>
2016-01-19 13:26:55 -0800

    1. Updating Variable-sample.ps1

--- Variables-sample.ps1
+++ Variables-sample.ps1
@@ -1,12 +1,12 @@
-﻿$Username          = "username"
-$Password          = "password" | ConvertTo-SecureString -AsPlainText -Force
-$DefaultCompany    = "company"
-$SearchBase        = "ou=ou,dc=dc,dc=dc"
-$Server            = "server"
-$Domain            = "domain"
+﻿$Username           = "username"
+$Password           = "password" | ConvertTo-SecureString -AsPlainText -Force
+$DefaultCompany     = "company"
+$SearchBase         = "ou=ou,dc=dc,dc=dc"
+$Server             = "server"
+$Domain             = "domain"
 $PathOfADUserFolder = "path of ADUser folder"
 $PathOfLogFile      = "path of log file"
-$FileSystemRight   = [System.Security.AccessControl.FileSystemRights]::FullControl
-$InheritanceFlag   = [System.Security.AccessControl.InheritanceFlags]::None
-$PropagationFlag   = [System.Security.AccessControl.PropagationFlags]::None
-$AccessControlType = [System.Security.AccessControl.AccessControlType]::Allow
+$FileSystemRight    = [System.Security.AccessControl.FileSystemRights]::FullControl
+$InheritanceFlag    = [System.Security.AccessControl.InheritanceFlags]::None
+$PropagationFlag    = [System.Security.AccessControl.PropagationFlags]::None
+$AccessControlType  = [System.Security.AccessControl.AccessControlType]::Allow
